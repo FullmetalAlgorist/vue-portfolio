5d0b25075c48d543e7d796c74f4fd63414282b95
akcrew <mariah.vicary@gmail.com>
2019-07-02 11:26:49 -0700

    Added catch block for history mode

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
